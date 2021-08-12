81b5d0bdd129922ad5786252370b236527921e92
Sam M <samstevenm@protonmail.com>
2021-08-11 20:43:43 -0700

    Update mm_upgrade.py

--- mm_upgrade.py
+++ mm_upgrade.py
@@ -11,6 +11,7 @@ logging.error("error")
 logging.critical("critical")
 
 def wait_for_enter():
+    print("\n")
     raw_input("Press Enter to continue.")
 
 class ExplainStep(object):
@@ -27,67 +28,62 @@ You can and should also read the guide [1](RTFM). Maybe this tool is useful.
 
 class SSHtoMMServerStep(object):
     def run(self, context):
-        print('''
-**SSH into Mattermost Server (Ubuntu)**
-        ''')
-        print("Run:")
-        print("ssh <USER>@<SERVER>")
+        print("**SSH into Mattermost Server (Ubuntu)**")
+        print("Run:\n")
+        print("   ssh <USER>@<SERVER>")
         wait_for_enter()
 
 class PrepUbuntuStep(object):
     def run(self, context):
-        print('''
-        **Completely update Mattermost Server (Ubuntu)**
-        ''')
-        print("Run:")
-        print("sudo apt update && sudo apt upgrade -y")
+        print("**Completely update Mattermost Server (Ubuntu)**")
+        print("Run:\n")
+        print("")
+        print("   sudo apt update && sudo apt upgrade -y")
         wait_for_enter()
-        print('''
-        **Remove unnecessary stuff from Mattermost Server (Ubuntu)**
-        ''')
-        print("Run:")
-        print("sudo apt-get autoremove --purge -y")
+
+        print("**Remove unnecessary stuff from Mattermost Server (Ubuntu)**")
+        print("Run:\n")
+        print("   sudo apt-get autoremove --purge -y")
         wait_for_enter()
-        print('''
-        **Free up space by vacuuming the logs (Ubuntu)**
-        ''')
-        print("Run:")
-        print("sudo journalctl --vacuum-time=3d")
+
+        print("**Free up space by vacuuming the logs (Ubuntu)**")
+        print("Run:\n")
+        print("   sudo journalctl --vacuum-time=3d")
         wait_for_enter()
-        print('''
-        **Make a note of disk usage (Ubuntu)**
-        ''')
-        print("Run:")
-        print("df -h")
-        context["remainingspace"] = raw_input("Enter the remaining space %: ")
+        print("**Make a note of disk usage (Ubuntu)**")
+        print("Run:\n")
+        print("   df -h")
+        print("\n")
+        context["remainingspace"] = raw_input("!->Enter the remaining space %: ")
         print("**Remaining space is %{0}**".format(context["remainingspace"]))
         logging.info("Remaining space is %{0}".format(context["remainingspace"]))
         wait_for_enter()
 
 class DumpMMSQLStep(object):
     def run(self, context):
-        print('''
-        Dump the mattermost database to a SQL file
-        ''')
+        print("**Dump the mattermost database to a SQL file**")
+        print("This next command will prompt you for your root password.")
         print("Run:")
-        print('''
-        mysqldump -u root -p mattermost > mattermost_backup$(date +"%Y-%m-%d_%H-%M-%S").sql
-        ''')
+        print("\n")
+        #There are escaped (")s - (\") in the printed line. This is important!
+        # The actual command is: mysqldump -u root -p mattermost > mattermost_backup$(date +"%Y-%m-%d_%H-%M-%S").sql
+        print("   mysqldump -u root -p mattermost > mattermost_backup$(date +\"%Y-%m-%d_%H-%M-%S\").sql")
         wait_for_enter()
 
 class WhereisMMStep(object):
     def run(self, context):
         print('''
-        **Find Mattermost binary**
-        If you do not know where Mattermost Server is installed,
-        use the `whereis mattermost` command. The output should be similar to
-        /opt/mattermost or /bin/mattermost.
-        The install directory is everything before the first occurrence
-        of the string /mattermost for example /opt or /bin.
+**Find Mattermost binary**
+If you do not know where Mattermost Server is installed, use the
+`whereis mattermost` command. The output should be similar to
+/opt/mattermost, /opt/mattermost/bin/mattermost/ or /bin/mattermost.
+The install directory is everything *before* the first occurrence
+of the string /mattermost for example /opt or /bin.
         ''')
-        print("Run:")
-        print("whereis mattermost")
-        context["mmlocation"] = raw_input("Enter the location (i.e. /opt): ")
+        print("Run:\n")
+        print("   whereis mattermost")
+        print("\n")
+        context["mmlocation"] = raw_input("!->Enter the location (i.e. /opt): ")
         print("**Mattermost is installed at {0}**".format(context["mmlocation"]))
         logging.info("Mattermost is installed at {0}".format(context["mmlocation"]))
         wait_for_enter()
@@ -95,22 +91,20 @@ class WhereisMMStep(object):
 class ChangeDirStep(object):
     def run(self, context):
         print('''
-        In a terminal window on the server that hosts Mattermost,
-        change to your /tmp directory. Delete any files and directories that might
-        still exist from a previous download.
+In a terminal window on the server that hosts Mattermost, change to your /tmp
+directory. Delete or rename any old Mattermost files and directories.
         ''')
-        print("Run:")
+        print("Run:\n")
         print("cd /tmp")
         wait_for_enter()
 
 class ConfirmDirEmpty(object):
     def run(self, context):
         print('''
-Confirm no other Mattermost zip folders exist in
-your /tmp directory. If other versions or files exist,
-delete or rename them.
+Confirm no other Mattermost zip folders exist in your /tmp directory. If other
+versions or files exist, delete or rename them.
               ''')
-        print("Run:")
+        print("Run:\n")
         print("ls -- mattermost*.gz")
         wait_for_enter()
 
@@ -118,32 +112,31 @@ class DownloadLatestVersionStep(object):
     dir_url = "https://mattermost.com/download/"
     def run(self, context):
         print("Go to {0}".format(self.dir_url))
-        context["downloadlink"] = raw_input("Paste the download link and press enter: ")
-        print("Run:")
-        print("wget {0}".format(context["downloadlink"]))
+        print("\n")
+        context["downloadlink"] = raw_input("!->Paste the download link and press enter: ")
+        print("Run:\n")
+        print("   wget {0}".format(context["downloadlink"]))
         wait_for_enter()
 
 class ExtractMMServerFilesStep(object):
     def run(self, context):
-        print("Run:")
-        #There is an escaped, escape \\ in the next line. This is important!
-        #The actual command is tar -xf mattermost*.gz --transform='s,^[^/]\+,\0-upgrade,'
-        print('''
-        tar -xf mattermost*.gz --transform='s,^[^/]\+,\\0-upgrade,'
-              ''')
+        print("Run:\n")
+        #There is an escaped (\) - (\\) in the next line. This is important!
+        #The actual command is: tar -xf mattermost*.gz --transform='s,^[^/]\+,\0-upgrade,'
+        print("   tar -xf mattermost*.gz --transform='s,^[^/]\+,\\0-upgrade,'")
         wait_for_enter()
 
 class StopMMServerStep(object):
     def run(self, context):
-        print("Run:")
-        print("sudo systemctl stop mattermost")
+        print("Run:\n")
+        print("   sudo systemctl stop mattermost")
         wait_for_enter()
 
 class BackupMMServerStep(object):
     def run(self, context):
-        print("Run:")
-        print("cd {0}".format(context["mmlocation"]))
-        print("sudo cp -ra mattermost/ mattermost-back-$(date +'%F-%H-%M')/")
+        print("Run:\n")
+        print("   cd {0}".format(context["mmlocation"]))
+        print("   sudo cp -ra mattermost/ mattermost-back-$(date +'%F-%H-%M')/")
         wait_for_enter()
 
 class CleanOldMMVersionStep(object):
@@ -151,9 +144,9 @@ class CleanOldMMVersionStep(object):
         print('''
 **Remove all files except data and custom directories from within the current mattermost directory.**
 ''')
-        print("Run:")
+        print("Run:\n")
         print('''
-sudo find mattermost/ mattermost/client/ -mindepth 1 -maxdepth 1 \! \( -type d \( -path mattermost/client -o -path mattermost/client/plugins -o -path mattermost/config -o -path mattermost/logs -o -path mattermost/plugins -o -path mattermost/data \) -prune \) | sort | xargs echo rm -r)
+   sudo find mattermost/ mattermost/client/ -mindepth 1 -maxdepth 1 \! \( -type d \( -path mattermost/client -o -path mattermost/client/plugins -o -path mattermost/config -o -path mattermost/logs -o -path mattermost/plugins -o -path mattermost/data \) -prune \) | sort | xargs echo rm -r)
     ''')
         wait_for_enter()
 
@@ -162,7 +155,7 @@ class ChangeOwnerNewMMStep(object):
         print('''
 **Change ownership of the new files before copying them.**
 ''')
-        print("Run:")
+        print("Run:\n")
         print('''
    sudo chown -hR mattermost:mattermost /tmp/mattermost-upgrade/
     ''')
@@ -173,7 +166,7 @@ class CopyCleanStep(object):
         print('''
 **Copy the new files to your install directory and remove the temporary files.**
 ''')
-        print("Run:")
+        print("Run:\n")
         print('''
    sudo cp -an /tmp/mattermost-upgrade/. mattermost/
    sudo rm -r /tmp/mattermost-upgrade/
@@ -189,7 +182,7 @@ If you want to use port 80 to serve your server, or if you have TLS set up on
 your Mattermost server, you must activate the CAP_NET_BIND_SERVICE capability to
  allow the new Mattermost binary to bind to low ports.
  ''')
-        print("Run:")
+        print("Run:\n")
         print('''
    cd {0}/mattermost
    sudo setcap cap_net_bind_service=+ep ./bin/mattermost
@@ -198,7 +191,7 @@ your Mattermost server, you must activate the CAP_NET_BIND_SERVICE capability to
 
 class StartMMServerStep(object):
     def run(self, context):
-        print("Run:")
+        print("Run:\n")
         print("sudo systemctl start mattermost")
         wait_for_enter()
 
